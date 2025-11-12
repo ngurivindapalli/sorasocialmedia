e96d0139419530745986f6532bad0ec27bc20b5f
Noel <nagurivindapalli@gmail.com>
2025-11-12 02:43:44 -0500

    Update build script to use binary wheels

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # Build script for Render
 
 cd backend
-pip install --upgrade pip
-pip install -r requirements.txt
+pip install --upgrade pip setuptools wheel
+pip install --no-cache-dir --only-binary=:all: -r requirements.txt || pip install --no-cache-dir -r requirements.txt
